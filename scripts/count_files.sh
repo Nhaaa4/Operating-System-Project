c109c61c91eec5aa9692c27bb25211c06435a6ff
Visa Huy <visadekh@gmail.com>
2025-03-28 15:01:38 +0700

    Visa: Add Count File

--- scripts/count_files.sh
+++ scripts/count_files.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
 
-read -p "Enter path of directory to count files: " path
-count=$(ls -1 "$path" | wc -l)
-echo "Number of files in directory '$path' is '$count'"
+# Define color codes
+RED='\033[0;31m'
+NC='\033[0m' 
+
+source scripts/logger.sh
+
+count_files() {
+    clear
+    echo "+----------------------------------------------+"
+    echo "|                COUNT FILES                   |"
+    echo -e "+----------------------------------------------+\n"
+
+    read -rp "Enter directory path: " dir
+    echo ""
+
+    if [[ ! -d "$dir" ]]; then
+        echo -e "${RED}Error: Directory does not exist.${NC}"
+        log_action "Failed to count files: $dir does not exist."
+        return 1
+    fi
+
+    count=$(find "$dir" -type f | wc -l)
+    echo "Total files in $dir: $count file(s)"
+    log_action "Counted $count files in $dir"
+}
